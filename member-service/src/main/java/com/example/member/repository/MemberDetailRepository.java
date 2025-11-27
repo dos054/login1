88d2a96e951cc17bef2d793ba87f31e3573eed00
dos054 <zaqsdf1234@gmail.com@github.com>
2025-11-27 18:36:23 +0900

    수정

--- member-service/src/main/java/com/example/member/repository/MemberDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.member.repository;
-
-import com.example.member.model.MemberDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// 회원 상세정보 데이터 접근 Repository (JPA)
-// - 현재 미사용, 추후 확장용
-public interface MemberDetailRepository extends JpaRepository<MemberDetail, Long> {
-}
